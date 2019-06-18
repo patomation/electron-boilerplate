c3077fe5327fb6089e8a2600cca31394b6555d0d
patomation <patomation@gmail.com>
2019-06-18 20:18:24 +0700

    moves source to src

--- src/main.js
+++ src/main.js
@@ -17,10 +17,10 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
