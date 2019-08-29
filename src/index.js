58054690429f77469713a831874b22ba40c82749
patomation <patomation@gmail.com>
2019-08-29 16:05:59 +0700

    Makes index example generic and fixes dependencies

--- src/index.js
+++ src/index.js
@@ -2,17 +2,8 @@
 import eventsGlobal from './globals/events.js';
 import styles from './sass/main.scss';
 
-import dragDropPreventDefault from './modules/dragDropPreventDefault.js';
-import dropZone from './modules/dropZone.js';
-import fileList from './modules/fileList.js';
-
 const entry = () => {
-  dragDropPreventDefault();
-  dropZone.init();
-  fileList.init();
-  events.on('drop',(files)=>{
-    debugger;
-  })
+
 }
 
 
