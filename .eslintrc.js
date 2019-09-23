ad970e8f8cad3ca913c3e4f463e10a7e2db54b6f
patomation <patomation@gmail.com>
2019-09-23 12:20:54 +0700

    .eslintrc formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,31 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
     }
-};
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
